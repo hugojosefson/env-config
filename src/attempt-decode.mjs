c04d7d65b10f1ef2381f6799716ca9fa8ea2a1a3
Hugo Josefson <hugo@josefson.org>
2020-05-17 17:08:10 +0000

    Refactor: Extract reducer.

--- src/attempt-decode.mjs
+++ src/attempt-decode.mjs
@@ -1,33 +1,35 @@
+import identity from './fn/identity.mjs'
 import pipe from './fn/pipe.mjs'
 import withoutPrefix from './fn/without-prefix.mjs'
 
-export default decoders => {
-  const attemptDecode = (value, ...seenValues) => {
-    if (decoders.length === 0) {
-      return value
+const attemptDecodeReducer = (
+  acc,
+  {
+    prefix,
+    test = s => typeof s === 'string' && s.startsWith(prefix),
+    decodeWithoutPrefix,
+    decode = pipe(withoutPrefix(prefix), decodeWithoutPrefix),
+  }
+) => {
+  try {
+    if (test(acc)) {
+      return decode(acc)
     }
-    const result = decoders.reduce(
-      (
-        acc,
-        {
-          prefix,
-          test = s => typeof s === 'string' && s.startsWith(prefix),
-          decodeWithoutPrefix,
-          decode = pipe(withoutPrefix(prefix), decodeWithoutPrefix),
-        }
-      ) => {
-        try {
-          if (test(acc)) {
-            return decode(acc)
-          }
-        } catch (ignore) {}
-        return acc
-      },
-      value
-    )
+  } catch (ignore) {}
+  return acc
+}
+
+export default decoders => {
+  if (decoders.length === 0) {
+    return identity
+  }
+
+  const attemptDecode = (value, ...seenValues) => {
+    const result = decoders.reduce(attemptDecodeReducer, value)
 
     if (result === value) return result
     if (seenValues.includes(result)) return result
+
     return attemptDecode(result, value, ...seenValues)
   }
 
